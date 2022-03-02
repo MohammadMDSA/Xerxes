13894a88606d5ee24886240c58c2de74a290ef40
Mohammad MDSA <samimd.77@gmail.com>
2022-03-02 10:01:40 +0330

    Add mouse helpers

--- Xerxes.Engine/InputManager.cpp
+++ Xerxes.Engine/InputManager.cpp
@@ -4,18 +4,115 @@
 using namespace std;
 using namespace DirectX;
 
-InputManager::InputManager()
+InputManager::InputManager() :
+	mouseX(0.f),
+	mouseY(0.f),
+	mouseDeltaX(0.f),
+	mouseDeltaY(0.f)
 {
-	mouse = make_shared<Mouse>();
+	mouse = unique_ptr<Mouse>();
 	keyboard = make_shared<Keyboard>();
 }
 
-std::shared_ptr<DirectX::Mouse> InputManager::GetMouse()
+DirectX::Mouse* InputManager::GetMouse()
 {
-	return mouse;
+	return mouse.get();
 }
 
 std::shared_ptr<DirectX::Keyboard> InputManager::GetKeyboard()
 {
 	return keyboard;
+}
+
+float InputManager::GetMouseX()
+{
+	return mouseX;
+}
+
+float InputManager::GetMouseY()
+{
+	return mouseY;
+}
+
+float InputManager::GetMouseDeltaX()
+{
+	return mouseDeltaX;
+}
+
+float InputManager::GetMouseDeltaY()
+{
+	return mouseDeltaY;
+}
+
+bool InputManager::GetLeftButton()
+{
+	return leftButton;
+}
+
+bool InputManager::GetRightButton()
+{
+	return rightButton;
+}
+
+bool InputManager::GetMiddleButton()
+{
+	return middleButton;
+}
+
+bool InputManager::GetLeftButtonUp()
+{
+	return leftButtonUp;
+}
+
+bool InputManager::GetRightButtonUp()
+{
+	return rightButtonUp;
+}
+
+bool InputManager::GetMiddleButtonUp()
+{
+	return middleButtonUp;
+}
+
+bool InputManager::GetLeftButtonDown()
+{
+	return leftButtonDown;
+}
+
+bool InputManager::GetRightButtonDown()
+{
+	return rightButtonDown;
+}
+
+bool InputManager::GetMiddleButtonDown()
+{
+	return middleButtonDown;
+}
+
+void InputManager::Update()
+{
+	auto mouseState = mouse->GetState();
+	mouseTracker.Update(mouseState);
+	leftButton = mouseState.leftButton;
+	rightButton = mouseState.rightButton;
+	middleButton = mouseState.middleButton;
+	leftButtonUp = mouseTracker.leftButton == Mouse::ButtonStateTracker::RELEASED;
+	rightButtonUp = mouseTracker.rightButton == Mouse::ButtonStateTracker::RELEASED;
+	middleButtonUp = mouseTracker.middleButton == Mouse::ButtonStateTracker::RELEASED;
+	leftButtonDown = mouseTracker.leftButton == Mouse::ButtonStateTracker::PRESSED;
+	rightButtonDown = mouseTracker.rightButton == Mouse::ButtonStateTracker::PRESSED;
+	middleButtonDown = mouseTracker.middleButton == Mouse::ButtonStateTracker::PRESSED;
+	if (mouseState.positionMode == Mouse::MODE_ABSOLUTE)
+	{
+		mouse->SetMode(Mouse::MODE_RELATIVE);
+	}
+	else
+	{
+		auto newX = mouseState.x;
+		auto newY = mouseState.y;
+		mouseDeltaX = newX - mouseX;
+		mouseDeltaY = newY - mouseY;
+		mouseX = newX;
+		mouseY = newY;
+	}
 }
--- Xerxes.Engine/InputManager.h
+++ Xerxes.Engine/InputManager.h
@@ -6,11 +6,43 @@ class InputManager
 public:
 	InputManager();
 
-	std::shared_ptr<DirectX::Mouse>		GetMouse();
+	DirectX::Mouse*						GetMouse();
 	std::shared_ptr<DirectX::Keyboard>	GetKeyboard();
+	float								GetMouseX();
+	float								GetMouseY();
+	float								GetMouseDeltaX();
+	float								GetMouseDeltaY();
+	bool								GetLeftButton();
+	bool								GetRightButton();
+	bool								GetMiddleButton();
+	bool								GetLeftButtonUp();
+	bool								GetRightButtonUp();
+	bool								GetMiddleButtonUp();
+	bool								GetLeftButtonDown();
+	bool								GetRightButtonDown();
+	bool								GetMiddleButtonDown();
+
+	void								Update();
 
 private:
-	std::shared_ptr<DirectX::Mouse>		mouse;
+	std::unique_ptr<DirectX::Mouse>		mouse;
 	std::shared_ptr<DirectX::Keyboard>	keyboard;
+	DirectX::Mouse::ButtonStateTracker	mouseTracker;
+
+	float								mouseX;
+	float								mouseY;
+	float								mouseDeltaX;
+	float								mouseDeltaY;
+
+	bool								leftButton;
+	bool								leftButtonUp;
+	bool								leftButtonDown;
+	bool								rightButton;
+	bool								rightButtonUp;
+	bool								rightButtonDown;
+	bool								middleButton;
+	bool								middleButtonUp;
+	bool								middleButtonDown;
+
 };
 
