1181ddac09ab0009274b6220560b498f7f32814b
Mohammad MDSA <samimd.77@gmail.com>
2022-03-27 02:41:42 +0430

    Add particle shaders

---
+++ Xerxes.Engine/Shaders/Particle_PS.hlsl
@@ -0,0 +1,28 @@
+// GLOBALS //
+Texture2D shaderTexture;
+SamplerState SampleType;
+
+// TYPEDEFS //
+struct PixelInputType
+{
+    float4 position : SV_POSITION;
+    float2 tex : TEXCOORD0;
+    float4 color : COLOR;
+};
+
+
+float4 main(PixelInputType input) : SV_TARGET
+{
+    float4 textureColor;
+    float4 finalColor;
+
+
+    // Sample the pixel color from the texture using the sampler at this texture coordinate location.
+    textureColor = shaderTexture.Sample(SampleType, input.tex);
+
+    // Combine the texture color and the particle color to get the final color result.
+    //finalColor = input.color;
+    finalColor = textureColor * input.color;
+
+    return finalColor;
+}
