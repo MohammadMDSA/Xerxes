08f2678b2adfa1311e5cdf52defdc0329e781c35
Mohammad MDSA <samimd.77@gmail.com>
2022-04-18 22:59:18 +0430

    Add gui texture selection helper macro

--- Xerxes.Engine/TextureResource.h
+++ Xerxes.Engine/TextureResource.h
@@ -1,6 +1,39 @@
 #pragma once
 #include "GameResource.h"
 
+#define XDrawTextureInspect(fidName, fName) \
+{ \
+	auto resourceManager = XResourceM(); \
+	auto availableWidth = ImGui::GetContentRegionAvail().x; \
+	auto texResource = resourceManager->ResourceGroup<TextureResource>::GetById(fidName); \
+	std::string resourceName = texResource ? texResource->GetName() : "[select texture]"; \
+	if(ImGui::Button(resourceName.c_str(), ImVec2(0.6f * availableWidth, 0))) \
+		ImGui::OpenPopup(#fName "TextureSelection"); \
+	ImGui::SameLine(); \
+	ImGui::Text(#fName ""); \
+	if (ImGui::BeginPopup(#fName "TextureSelection"))\
+	{\
+		auto textures = resourceManager->ResourceGroup<TextureResource>::GetAll(); \
+		if (ImGui::Selectable("<none>")) \
+		{ \
+			fidName = -1; \
+		} \
+		int i = 0;\
+		for (auto it : textures) \
+		{ \
+			ImGui::PushID(++i); \
+			if (ImGui::Selectable((it->GetName() + " (" + it->GetType() + ")").c_str())) \
+			{ \
+				fidName = it->GetId(); \
+			} \
+			ImGui::PopID(); \
+		} \
+		ImGui::EndPopup(); \
+	} \
+	ImGui::Spacing(); \
+}
+
+
 class TextureResource : public GameResource<ID3D11ShaderResourceView>
 {
 public:
